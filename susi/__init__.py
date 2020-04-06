67ef233b81a26b3f0bb1544e8d57f6129a316524
Felix Riese <felix.riese@kit.edu>
2020-04-06 23:34:14 +0200

    Add new plot scripts

--- susi/__init__.py
+++ susi/__init__.py
@@ -8,5 +8,6 @@ self-organizing maps (SOM).
 from .SOMClassifier import SOMClassifier
 from .SOMClustering import SOMClustering
 from .SOMEstimator import SOMEstimator
+from .SOMPlots import *
 from .SOMRegressor import SOMRegressor
 from .SOMUtils import *
