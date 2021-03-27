1a7cd4b4e1efb756b1e89bd551af1e310d4fc172
Felix M. Riese <mail@felixriese.de>
2021-03-27 12:15:12 +0100

    Add extra requirements, fix rst read-in

--- setup.py
+++ setup.py
@@ -7,8 +7,8 @@ import setuptools
 with open("README.rst", "r") as f:
     readme = f.read()
 
-with open('CHANGELOG.rst', 'rb') as f:
-    changelog = f.read().decode('utf-8')
+with open('CHANGELOG.rst', 'r') as f:
+    changelog = f.read()
 
 long_description = '\n\n'.join((readme, changelog))
 
@@ -17,7 +17,8 @@ setuptools.setup(
     version="1.1.2",
     author="Felix M. Riese",
     author_email="github@felixriese.de",
-    description="Python package for unsupervised, supervised and semi-supervised self-organizing maps (SOM)",
+    description=("Python package for unsupervised, supervised and "
+                 "semi-supervised self-organizing maps (SOM)"),
     long_description=long_description,
     long_description_content_type="text/x-rst",
     url="https://github.com/felixriese/susi",
@@ -29,7 +30,10 @@ setuptools.setup(
                       "tqdm",
                       "matplotlib"],
     extras_require={"docs": ["numpydoc", "sphinx", "sphinx-autobuild",
-                             "sphinx_rtd_theme"]},
+                             "sphinx_rtd_theme"],
+                    "examples": ["notebook", "seaborn", "pandas"],
+                    "tests": ["pytest", "pytest-cov", "codecov", "nbval",
+                              "coverage"]},
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
